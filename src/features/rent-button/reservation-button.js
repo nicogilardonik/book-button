8d89817579f8b769de99f7fa0ea2539b5c01c444
Nico Gilardoni <nicogilardonik@gmail.com>
2025-02-07 12:46:17 -0300

    feat: add shadow DOM support and integrate Tailwind CSS into ReservationButton

--- src/features/rent-button/reservation-button.js
+++ src/features/rent-button/reservation-button.js
@@ -10,4 +10,18 @@ const customReactDOM = {
 };
 console.log('RentButton', RentButton);
 const WebReservationButton = reactToWebComponent(RentButton, React, customReactDOM);
+
+WebReservationButton.prototype.connectedCallback = function () {
+    if (!this.shadowRoot) {
+        this.attachShadow({ mode: 'open' });
+    }
+
+    const style = document.createElement('style');
+    style.textContent = `
+        @import url('../../tailwind.css');
+    `;
+
+    this.shadowRoot.prepend(style);
+};
+
 customElements.define('reservation-button', WebReservationButton);
