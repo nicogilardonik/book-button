3a021731da3e7e40e7b4064afb9e3f25b81ebc7f
Nico Gilardoni <nicogilardonik@gmail.com>
2025-02-07 12:49:37 -0300

    refactor: remove redundant shadow DOM setup from WebReservationButton

--- src/features/rent-button/reservation-button.js
+++ src/features/rent-button/reservation-button.js
@@ -8,20 +8,6 @@ const customReactDOM = {
         createRoot(container).render(element);
     }
 };
-console.log('RentButton', RentButton);
+
 const WebReservationButton = reactToWebComponent(RentButton, React, customReactDOM);
-
-WebReservationButton.prototype.connectedCallback = function () {
-    if (!this.shadowRoot) {
-        this.attachShadow({ mode: 'open' });
-    }
-
-    const style = document.createElement('style');
-    style.textContent = `
-        @import url('../../tailwind.css');
-    `;
-
-    this.shadowRoot.prepend(style);
-};
-
 customElements.define('reservation-button', WebReservationButton);
